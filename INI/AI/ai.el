7587eb4ffc0b209e530a10f40754475085b3c670
buyn <buyn.tex@gmail.com>
2024-08-06 01:54:34 +0300

    desblet gpt in ai.el

--- INI/AI/ai.el
+++ INI/AI/ai.el
@@ -1,19 +1,20 @@
 (use-package gpt
- :ensure t
- :config
-  ;; (setq gpt-openai-key "sk-*")
-  (setq gpt-openai-engine "gpt-3.5-turbo")
-  (define-key evil-normal-state-map (kbd "M-a M-j M-o") 'gpt-dwim)
+	:disabled
+	:ensure t
+	:config
+		;; (setq gpt-openai-key "sk-*")
+		(setq gpt-openai-engine "gpt-3.5-turbo")
+		(define-key evil-normal-state-map (kbd "M-a M-j M-o") 'gpt-dwim)
   )
 
 (use-package gptel
-	:ensure t
 	;; :disabled
+	:ensure t
 	:config
-  (custom-set-variables '(gptel-model "gpt-3.5-turbo-16k"))
-  (define-key evil-normal-state-map (kbd "M-a M-j M-S-h") 'gptel-system-prompt)
-  (define-key evil-normal-state-map (kbd "M-a M-j M-j") 'gptel-send)
-  (define-key evil-normal-state-map (kbd "M-a M-j M-h") 'gptel-mode)
-  ;; (setq gpt-openai-key "sk-*")
-  (setq gptel-default-mode 'org-mode)
+		(custom-set-variables '(gptel-model "gpt-3.5-turbo-16k"))
+		(define-key evil-normal-state-map (kbd "M-a M-j M-S-h") 'gptel-system-prompt)
+		(define-key evil-normal-state-map (kbd "M-a M-j M-j") 'gptel-send)
+		(define-key evil-normal-state-map (kbd "M-a M-j M-h") 'gptel-mode)
+		;; (setq gpt-openai-key "sk-*")
+		(setq gptel-default-mode 'org-mode)
 )
