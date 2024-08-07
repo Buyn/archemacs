3602a704ea1e5d372ef7974e79ee6d7cffc65d00
buyn <buyn.tex@gmail.com>
2024-08-07 20:27:00 +0300

    add parifer\rust mode and disabled

--- INI/dev/delimiters-cfg.el
+++ INI/dev/delimiters-cfg.el
@@ -43,4 +43,48 @@
 ;; Stop Highlighting Certain Delimiters
 
 ;; You can disable highlighting of any of the supported delimiter types through the customize interface, in the Toggle Delimiters section.
+;; ** parinfer
+;; *** install
+(use-package parinfer
+  :disabled
+	:defer t
+  :ensure t
+  :bind
+  (("C-," . parinfer-toggle-mode))
+  :init
+  (progn
+    (setq parinfer-extensions
+          '(defaults       ; should be included.
+            pretty-parens  ; different paren styles for different modes.
+            evil           ; If you use Evil.
+            lispy          ; If you use Lispy. With this extension, you should install Lispy and do not enable lispy-mode directly.
+            paredit        ; Introduce some paredit commands.
+            smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
+            smart-yank))   ; Yank behavior depend on mode.
+    (add-hook 'clojure-mode-hook #'parinfer-mode)
+    (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
+    (add-hook 'common-lisp-mode-hook #'parinfer-mode)
+    (add-hook 'scheme-mode-hook #'parinfer-mode)
+    (add-hook 'lisp-mode-hook #'parinfer-mode))
+;; *** END )
+	)
+;; ** parinfer-rust-mode
+;; *** install
+(use-package parinfer-rust-mode
+  ;; :disabled
+	:defer t
+	:ensure t
+	;; :hook emacs-lisp-mode
+	;; :init
+		;; (setq parinfer-rust-auto-download t)
+  :config
+		(setq  parinfer-rust-disable-troublesome-modes t)
+    ;; (parinfer-rust-mode -1)
+    ;; (add-hook 'clojure-mode-hook #'parinfer-rust-mode)
+    ;; (add-hook 'emacs-lisp-mode-hook #'parinfer-rust-mode)
+    ;; (add-hook 'common-lisp-mode-hook #'parinfer-rust-mode)
+    ;; (add-hook 'scheme-mode-hook #'parinfer-rust-mode)
+    ;; (add-hook 'lisp-mode-hook #'parinfer-rust-mode)
+;; *** END )
+	)
 ;; * --------------------------------------
