208629fe3ce63b11dfb395e94b935a470a9bafa8
buyn <buyn.tex@gmail.com>
2024-08-11 06:07:56 +0300

    refactpre clojure cfg

--- INI/dev/clojure-cfg.el
+++ INI/dev/clojure-cfg.el
@@ -12,7 +12,7 @@
 ;; M-x package-install RET rainbow-delimiters RET
 ;; (unless (package-installed-p 'clojure-mode)
 ;;   (package-install 'clojure-mode))
-(require 'use-package)
+;; (require 'use-package)
 (use-package clojure-mode
 	:ensure t
 	;; :disabled
@@ -30,5 +30,21 @@
 ;; **** links:
 ;; https://github.com/clojure-emacs/clojure-mode
 ;; **** Customization:
+;; *** --------------------------------------
+;; ** ob-clojur-mode
+;; *** install
+(use-package ob-clojure
+	;; :disabled
+	:defer t
+	;; :ensure t
+	:config
+		(setq org-babel-clojure-backend nil)
+	;; (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+	;; (custom-theme-set-faces
+	;; 	'material
+	;; 	`(rainbow-delimiters-depth-1-face ((t
+	;; 			(:foreground ,"aquamarine"))))
+	;; 	)
+	)
 ;; *** --------------------------------------
 ;; * --------------------------------------
