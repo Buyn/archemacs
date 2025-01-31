e732cf1037d1ce834f470e6862ac71ef8fa98be5
buyn <buyn.tex@gmail.com>
2025-02-01 01:39:50 +0200

    fix grammer-mod ispell hunspell

--- INI/lit-cfg/grammer-cfg.el
+++ INI/lit-cfg/grammer-cfg.el
@@ -13,7 +13,7 @@
 (use-package ispell   
 	:config   
   ;; :init
-		(setq ispell-program-name "~/AddApps/hunspell-1.3.2-3-w32-bin/bin/hunspell.exe")   
+		;; (setq ispell-program-name "~/AddApps/hunspell-1.3.2-3-w32-bin/bin/hunspell.exe")   
 		;; (setq ispell-program-name "D:/Development/lisp/HOME/AddApps/hunspell-1.3.2-3-w32-bin/bin/hunspell.exe")   
 	)
 ;; **** Docs
