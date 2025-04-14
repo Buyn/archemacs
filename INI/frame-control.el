2edf2121282dd662d52f55f95efaf0805d98eb14
buyn <buyn.tex@gmail.com>
2025-04-14 10:13:23 +0300

    refactor outlined framecontrol in org

--- INI/frame-control.el
+++ INI/frame-control.el
@@ -1,6 +1,3 @@
-;; * WINDMOVE&FRAMEMOVE SETUP{{{
-;; --------------------------------------
-;; ** Seting up
 (require 'cl) ;;used to Fix error if theres an error with above code
 ;; (load "~/.archemacs/ELs/framemove/framemove.el")
 (load (expand-file-name "ELs/framemove/framemove.el" user-emacs-directory))
@@ -10,8 +7,7 @@
 ;; Common lisp stuff to fix windmove/framemove 
 ;; (when (fboundp 'windmove-default-keybindings)
 ;;   (windmove-default-keybindings))
-;; --------------------------------------
-;; ** Remap windmove keys to home keys
+
 (global-set-key (kbd "M-h") nil)
 (global-set-key (kbd "M-h") 'windmove-left)
 (define-key org-mode-map (kbd "M-h") 'windmove-left)
@@ -25,11 +21,17 @@
 (global-set-key (kbd "M-k") 'windmove-up)
 (global-set-key (kbd "M-l") nil)
 (global-set-key (kbd "M-l") 'windmove-right)
-;; --------------------------------------
-;; --------------------------------------
-;; * FRAME SIZE&POSITION CONTROL
-;; ** FRAME POSITION LIST
-;; *** MONITOR 1 FRAME POSITION LIST
+
+(setq buyn-monitr-0-position-7 '(0     337))
+(setq buyn-monitr-0-position-8 '(670   337))
+(setq buyn-monitr-0-position-9 '(660   337))
+(setq buyn-monitr-0-position-4 '(0     600))
+(setq buyn-monitr-0-position-5 '(670   600))
+(setq buyn-monitr-0-position-6 '(660   600))
+(setq buyn-monitr-0-position-1 '(0     910))
+(setq buyn-monitr-0-position-2 '(670   910))
+(setq buyn-monitr-0-position-3 '(660   910))
+
 (setq buyn-monitr-1-position-7 '(0     0))
 (setq buyn-monitr-1-position-8 '(670   0))
 (setq buyn-monitr-1-position-9 '(1295  0))
@@ -39,7 +41,7 @@
 (setq buyn-monitr-1-position-1 '(0     620))
 (setq buyn-monitr-1-position-2 '(670   620))
 (setq buyn-monitr-1-position-3 '(1295  620))
-;; *** MONITOR 2 FRAME POSITION LIST
+
 (setq buyn-monitr-2-position-7 '(1920  0))
 (setq buyn-monitr-2-position-8 '(2115  0))
 (setq buyn-monitr-2-position-9 '(2310  0))
@@ -49,8 +51,7 @@
 (setq buyn-monitr-2-position-1 '(1920  752))
 (setq buyn-monitr-2-position-2 '(2115  752))
 (setq buyn-monitr-2-position-3 '(2310  752))
-;; ** FRAME POSITION CONTROL KEYS
-;; *** MONITOR 0
+
 (global-set-key (kbd "M-S-<kp-home>") '(lambda() (interactive)
       (set-frame-position (selected-frame)
                           (car buyn-monitr-0-position-7)
@@ -87,7 +88,7 @@
       (set-frame-position (selected-frame)
                           (car buyn-monitr-0-position-3)
                           (cadr buyn-monitr-0-position-3))))
-;; *** MONITOR 1
+
 (global-set-key (kbd "M-<kp-7>") '(lambda() (interactive)
       (set-frame-position (selected-frame)
                           (car buyn-monitr-1-position-7)
@@ -124,7 +125,7 @@
       (set-frame-position (selected-frame)
                           (car buyn-monitr-1-position-3)
                           (cadr buyn-monitr-1-position-3))))
-;; *** MONITOR 2
+
 (global-set-key (kbd "C-M-<kp-7>") '(lambda() (interactive)
       (set-frame-position (selected-frame)
                           (car buyn-monitr-2-position-7)
@@ -161,14 +162,12 @@
       (set-frame-position (selected-frame)
                           (car buyn-monitr-2-position-3)
                           (cadr buyn-monitr-2-position-3))))
-;; ** FRAME SIZE CONTROL KEYS
+
 (global-set-key (kbd "M-<kp-subtract>") '(lambda() (interactive)
         (set-frame-size (selected-frame) 75 25)))
 (global-set-key (kbd "C-M-<kp-subtract>") '(lambda() (interactive)
         (set-frame-size (selected-frame) 75 35)))
-;; --------------------------------------
-;; * FRAMES&BUFFER OPEN\CLOSE\SWITCH   {{{
-;; ** Open new frames 
+
 (global-set-key (kbd "M-g M-t M-t") '(lambda() (interactive)
       (switch-to-buffer-other-frame "*scratch*")))
 (global-set-key (kbd "M-g M-t M-T") 'make-frame-command)
@@ -186,18 +185,12 @@
       (let (buffer-name-to-close (buffer-name))
       (evil-quit)
       (switch-to-buffer-other-frame buffer-name-to-close))))
-;; --------------------------------------
-;; ** Help in New Frame   ***  {{{
-;; (define-key evil-normal-state-map (kbd " M-g M-t M-j") 'other-frame)
-;; (define-key evil-normal-state-map (kbd " M-g M-t M-k") 'previous-multiframe-window)
-;; ;;(define-key evil-normal-state-map "gth" 'other-frame -2)
-;;(define-key evil-normal-state-map "gtl" 'other-frame 2)
+
 (global-set-key (kbd " M-g M-t M-h M-l") 'find-function-on-key-other-frame)
 (global-set-key (kbd " M-g M-t M-h M-k") 'find-function-on-key-other-frame)
 (global-set-key (kbd " M-g M-t M-h M-f") 'describe-function)
 (global-set-key (kbd " M-g M-t M-h M-a") 'xref-find-definitions-other-frame)
-;; --------------------------------------
-;; ** FRAMS CLOSE  {{{
+
 (define-key evil-normal-state-map "ZD" '(lambda() (interactive)
           (kill-buffer (current-buffer))))
 (define-key evil-normal-state-map "ZX" '(lambda() (interactive)
@@ -221,7 +214,6 @@
           (kill-buffer (current-buffer))
           ))
 
-;; ** Split in new window {{{
 (define-key evil-normal-state-map "ZHH" '(lambda() (interactive)
           (evil-window-vsplit)
           (ivy-switch-buffer)))
@@ -251,10 +243,9 @@
             (ivy-switch-buffer)))
 (define-key evil-normal-state-map (kbd "Z S-<SPC> S-<SPC>") 'delete-other-windows)
 
-;; ** Buffer control
 (define-key evil-normal-state-map "ZI" '(lambda() (interactive)
           (ivy-switch-buffer)))
-;; *** ranger Buffer control
+
 (define-key evil-normal-state-map "ZR" nil)
 (define-key evil-normal-state-map "ZRR" 'ranger)
 (define-key evil-visual-state-map "ZRR" '(lambda() (interactive)
@@ -291,7 +282,7 @@
             '(lambda() (interactive)
               (switch-to-buffer-other-frame "*scratch*")
               (ranger "~/Documents/")))
-;; *** magit Buffer control
+
 (define-key evil-normal-state-map "ZMm" '(lambda() (interactive)
                                            (if
                                                (boundp 'buyn-magit-buff-name)
@@ -311,38 +302,22 @@
                                            (magit-status user-emacs-directory)))
 (define-key evil-normal-state-map "ZMDS" '(lambda() (interactive)
                                            (buyn-shell-start "emacs --debug-init")))
-;; (define-key evil-normal-state-map "Zh" '(lambda() (interactive)
-;;          ;; (previous-buffer)))
-;;          (evil-prev-buffer)))
-;; (define-key evil-normal-state-map "Zl" '(lambda() (interactive)
-;;          (evil-next-buffer)))
-;; *** find-file Buffer control
+
 (define-key evil-normal-state-map "ZFF" 'find-file-other-frame)
 (define-key evil-visual-state-map "ZFF" '(lambda() (interactive)
           (find-file-other-frame (buffer-substring (region-beginning) (region-end)))))
 (define-key evil-normal-state-map "ZFI" '(lambda() (interactive)
           (find-file-other-frame (expand-file-name "INI/frame-control.el" user-emacs-directory))))
-;; (define-key evil-normal-state-map "Zk" '(lambda() (interactive)
-;;          (unbury-buffer)))
-;; (define-key evil-normal-state-map "Zj" '(lambda() (interactive)
-;;          (bury-buffer)))
-;; *** new Buffer
+
 (define-key evil-normal-state-map "ZNN" '(lambda() (interactive)
-      ;; (make-frame-command)
       (switch-to-buffer-other-frame "*scratch*")
-      ;; (evil-buffer-new 0 "*new*")))
       (evil-buffer-new "*new*")))
 (define-key evil-normal-state-map "ZNn" '(lambda() (interactive)
           (evil-buffer-new "*new*")))
-;; --------------------------------------
-;; * FULLSCREEN-MOD CUSTOMIZATION;  {{{
+
 ;; (find-file-other-frame "~/.archemacs/INI/fullscreen-cfg.el")
-;; (load "~/.archemacs/INI/fullscreen-cfg.el")
 (load (expand-file-name "INI/fullscreen-cfg.el" user-emacs-directory))
-;; --------------------------------------
-; }}}
-;; --------------------------------------
-;; * FRAMES-FUN CUSTOMIZATION  
+
 (defun buyn-get-frame-by-name (fname)
   "If there is a frame named FNAME, return it, else nil."
   (require 'dash) ; For `-some'
@@ -350,6 +325,3 @@
            (when (equal fname (frame-parameter frame 'name))
              frame))
          (frame-list)))
-;; --------------------------------------
-; }}}
-;; --------------------------------------
