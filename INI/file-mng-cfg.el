8de9636b6a788bf43b8ee3f095c0399b50a3d217
buyn <buyn.tex@gmail.com>
2025-04-16 03:35:17 +0300

    fix ranger C-S-F

--- INI/file-mng-cfg.el
+++ INI/file-mng-cfg.el
@@ -11,6 +11,7 @@
               ("g g" . ranger-goto-top)
               ("G G" . ranger-goto-bottom)
               ("Z F F" . find-file-other-frame)
+              ("C-S-F" . toggle-frame-maximized)
               ("Z D D" . mkdir)
               ("Z F I" . (lambda() (interactive)
                            (find-file-other-frame
