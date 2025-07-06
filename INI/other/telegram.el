079ba689c1f14860b4f8adea73b4759b90b91c60
buyn <buyn.tex@gmail.com>
2025-07-06 12:21:02 +0300

    Add telegram

---
+++ INI/other/telegram.el
@@ -0,0 +1,12 @@
+(use-package telega
+  ;; :disabled
+  :ensure t
+  :defer t
+
+:config
+(setq telega-server-libs-prefix "/usr")
+;; telega-chat-mode
+;; (define-key evil-normal-state-map (kbd "M-f") 'avy-goto-word-1)
+;; (define-key evil-normal-state-map (kbd "M-F") 'avy-goto-char-timer)
+
+)
