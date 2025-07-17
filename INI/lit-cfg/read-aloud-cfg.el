9439572c408b6f6a9c62881d5ab2c8aefec5e956
buyn <buyn.tex@gmail.com>
2025-07-17 18:49:43 +0300

    add aloud  tangle and fix for en

--- INI/lit-cfg/read-aloud-cfg.el
+++ INI/lit-cfg/read-aloud-cfg.el
@@ -2,4 +2,12 @@
   :ensure t
   :defer t
   :load-path "ELs/text-to-speech/read-aloud/"
+  :config
+    (lax-plist-put read-aloud-engines "spd.ru.irina"
+      '(cmd "spd-say"
+        args ("-t" "FEMALE1" "-l" "ru" "-e"))) 
+
+    (lax-plist-put read-aloud-engines "spd.en.hfc_female"
+      '(cmd "spd-say"
+        args ("-t" "FEMALE1" "-l" "en" "-e"))) 
   )
