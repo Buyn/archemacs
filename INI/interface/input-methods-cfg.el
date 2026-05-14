bd520fee645ac907be0d27d0e2523cc936ef5258
buyn <buyn.tex@gmail.com>
2026-05-15 00:43:01 +0300

    switch reys C-U M-U

--- INI/interface/input-methods-cfg.el
+++ INI/interface/input-methods-cfg.el
@@ -10,12 +10,15 @@
   :config
   (reverse-im-mode t)
 ;; ** define-key:
-  ;; (define-key evil-visual-state-map (kbd "M-U") 'reverse-im-translate-region)
+  (define-key evil-normal-state-map (kbd "M-U") 'toggle-input-method)
   (define-key evil-visual-state-map (kbd "M-U") '(lambda () (interactive) 
             (reverse-im-translate-region (region-beginning) (region-end))
             (toggle-input-method)))
-  (define-key evil-insert-state-map (kbd "M-U") 'reverse-im-translate-word)
+  (define-key evil-insert-state-map (kbd "C-M-u") 'reverse-im-translate-word)
   (define-key evil-insert-state-map (kbd "C-U") '(lambda () (interactive) 
+            (reverse-im-translate-word 1)
+            (toggle-input-method)))
+  (define-key evil-insert-state-map (kbd "M-U") '(lambda () (interactive) 
             (reverse-im-translate-word 1)
             (toggle-input-method)))
 ;; ** use-package END)
